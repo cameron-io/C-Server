c0ea17bc8f8db625c765a361b6269a662f0aca9b
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-20 02:13:18 +0000

    add file-mapper implementation to epoll version

--- app/src/main-epoll.cc
+++ app/src/main-epoll.cc
@@ -11,6 +11,125 @@
 constexpr int MAX_EVENTS = 10;
 constexpr int MAX_CLIENTS = 10;
 constexpr int PORT = 8080;
+constexpr char* base_path = "static";
+
+const char *get_content_type(const char* path) {
+    const char *last_dot = strrchr(path, '.');
+    if (last_dot) {
+        if (strcmp(last_dot, ".css") == 0) return "text/css";
+        if (strcmp(last_dot, ".csv") == 0) return "text/csv";
+        if (strcmp(last_dot, ".gif") == 0) return "image/gif";
+        if (strcmp(last_dot, ".htm") == 0) return "text/html";
+        if (strcmp(last_dot, ".html") == 0) return "text/html";
+        if (strcmp(last_dot, ".ico") == 0) return "image/x-icon";
+        if (strcmp(last_dot, ".jpeg") == 0) return "image/jpeg";
+        if (strcmp(last_dot, ".jpg") == 0) return "image/jpeg";
+        if (strcmp(last_dot, ".js") == 0) return "application/javascript";
+        if (strcmp(last_dot, ".json") == 0) return "application/json";
+        if (strcmp(last_dot, ".png") == 0) return "image/png";
+        if (strcmp(last_dot, ".pdf") == 0) return "application/pdf";
+        if (strcmp(last_dot, ".svg") == 0) return "image/svg+xml";
+        if (strcmp(last_dot, ".txt") == 0) return "text/plain";
+    }
+
+    return "application/octet-stream";
+}
+
+void send_400(int clientFd)
+{
+    const char *c400 = "HTTP/1.1 400 Bad Request\r\n"
+                       "Connection: close\r\n"
+                       "Content-Length: 11\r\n\r\nBad Request";
+    write(clientFd, c400, strlen(c400));
+}
+
+void send_404(int clientFd)
+{
+    const char *c404 = "HTTP/1.1 404 Not Found\r\n"
+                       "Connection: close\r\n"
+                       "Content-Length: 9\r\n\r\nNot Found";
+    write(clientFd, c404, strlen(c404));
+}
+
+void serve_resource(
+    int clientFd,
+    const char *path,
+    const char *base_path)
+{
+    if (strcmp(path, "/") == 0)
+        path = "/index.html";
+
+    if (strlen(path) > 100)
+    {
+        send_400(clientFd);
+        return;
+    }
+
+    if (strstr(path, ".."))
+    {
+        send_404(clientFd);
+        return;
+    }
+
+    char full_path[128];
+    sprintf(full_path, "%s%s", base_path, path);
+
+    FILE *fp = fopen(full_path, "rb");
+
+    if (!fp)
+    {
+        send_404(clientFd);
+        return;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    size_t cl = ftell(fp);
+    rewind(fp);
+
+    const char *ct = get_content_type(full_path);
+
+#define BSIZE 1024
+    char buffer[BSIZE];
+
+    sprintf(buffer, "HTTP/1.1 200 OK\r\n");
+    write(clientFd, buffer, strlen(buffer));
+
+    sprintf(buffer, "Connection: close\r\n");
+    write(clientFd, buffer, strlen(buffer));
+
+    sprintf(buffer, "Content-Length: %u\r\n", (unsigned int)cl);
+    write(clientFd, buffer, strlen(buffer));
+
+    sprintf(buffer, "Content-Type: %s\r\n", ct);
+    write(clientFd, buffer, strlen(buffer));
+
+    sprintf(buffer, "\r\n");
+    write(clientFd, buffer, strlen(buffer));
+
+    int r = fread(buffer, 1, BSIZE, fp);
+    while (r)
+    {
+        write(clientFd, buffer, r);
+        r = fread(buffer, 1, BSIZE, fp);
+    }
+
+    fclose(fp);
+}
+
+void handle_request(int clientFd, char* request) {
+    if (strncmp("GET /", request, 5)) {
+        send_400(clientFd);
+    } else {
+        char *path = request + 4;
+        char *end_path = strstr(path, " ");
+        if (!end_path) {
+            send_400(clientFd);
+        } else {
+            *end_path = 0;
+            serve_resource(clientFd, path, base_path);
+        }
+    }
+}
 
 class Client {
 public:
@@ -51,9 +170,7 @@ public:
             if (bytesRead <= 0) {
                 break;
             }
-            char * output_data = "Hello World!";
-            int output_data_len = strlen(output_data);
-            writeResponse(output_data, output_data_len);
+            handle_request(clientFd, input_buffer);
         }
 
         close(clientFd);
