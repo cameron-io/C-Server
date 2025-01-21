480c7699075516a94dea30c9ecf96853f17de3a2
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-21 14:21:36 +0000

    refactoring

--- lib/src/event_manager.cc
+++ lib/src/event_manager.cc
@@ -79,4 +79,147 @@ void EventManager::startEventLoop()
         }
     }
 }
+#else
+#include "event_manager.hh"
+
+void EventManager::startEventLoop()
+{
+    struct ClientInfo *clientList = 0;
+
+    while (true)
+    {
+        fd_set reads;
+        reads = this->waitOnClients(&clientList);
+
+        if (FD_ISSET(server->serverFd, &reads))
+        {
+            struct ClientInfo *client = this->getClient(&clientList, -1);
+
+            client->socket = accept(server->serverFd,
+                                    (struct sockaddr *)&(client->address),
+                                    &(client->addressLength));
+
+            if (!ISVALIDSOCKET(client->socket))
+            {
+                throw std::runtime_error("accept() failed.");
+            }
+        }
+
+        struct ClientInfo *client = clientList;
+        while (client)
+        {
+            struct ClientInfo *next = client->next;
+
+            if (FD_ISSET(client->socket, &reads))
+            {
+                if (MAX_REQUEST_SIZE == client->received)
+                {
+                    ResponseHandler::sendBadRequest(client->socket, "Request too large.");
+                    client = next;
+                    continue;
+                }
+
+                int r = recv(client->socket,
+                             client->request + client->received,
+                             MAX_REQUEST_SIZE - client->received, 0);
+
+                if (r < 1)
+                {
+                    this->dropClient(&clientList, client);
+                }
+                else
+                {
+                    client->received += r;
+                    client->request[client->received] = 0;
+
+                    char *q = strstr(client->request, "\r\n\r\n");
+                    if (q)
+                    {
+                        *q = 0;
+                        RequestHandler::handle(client->socket, client->request);
+                        this->dropClient(&clientList, client);
+                    }
+                }
+            }
+            client = next;
+        }
+    }
+}
+
+fd_set EventManager::waitOnClients(struct ClientInfo **clientList)
+{
+    fd_set reads;
+    FD_ZERO(&reads);
+    FD_SET(server->serverFd, &reads);
+    SOCKET maxSocket = server->serverFd;
+
+    struct ClientInfo *ci = *clientList;
+
+    while (ci)
+    {
+        FD_SET(ci->socket, &reads);
+        if (ci->socket > maxSocket)
+            maxSocket = ci->socket;
+        ci = ci->next;
+    }
+
+    if (select(maxSocket + 1, &reads, 0, 0, 0) < 0)
+    {
+        throw std::runtime_error("select() failed.");
+    }
+
+    return reads;
+}
+
+struct ClientInfo *EventManager::getClient(struct ClientInfo **clientList,
+                                           SOCKET s)
+{
+    struct ClientInfo *ci = *clientList;
+
+    while (ci)
+    {
+        if (ci->socket == s)
+            break;
+        ci = ci->next;
+    }
+
+    if (ci)
+        return ci;
+    struct ClientInfo *n =
+        (struct ClientInfo *)calloc(1, sizeof(struct ClientInfo));
+
+    if (!n)
+    {
+        fprintf(stderr, "Out of memory.\n");
+        exit(1);
+    }
+
+    n->addressLength = sizeof(n->address);
+    n->next = *clientList;
+    *clientList = n;
+    return n;
+}
+
+void EventManager::dropClient(struct ClientInfo **clientList,
+                              struct ClientInfo *client)
+{
+    CLOSESOCKET(client->socket);
+
+    struct ClientInfo **p = clientList;
+
+    while (*p)
+    {
+        if (*p == client)
+        {
+            *p = client->next;
+            free(client);
+            return;
+        }
+        p = &(*p)->next;
+    }
+
+    fprintf(stderr, "dropClient not found.\n");
+    exit(1);
+}
+
 #endif
