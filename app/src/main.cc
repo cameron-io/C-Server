2d90f2e106aadeb29e8191388c4c6ca8617a266e
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-22 11:50:10 +0000

    update app main, handle interrupts

--- app/src/main.cc
+++ app/src/main.cc
@@ -2,13 +2,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 #include "http_server.hh"
 #include "event_manager.hh"
 #include "request_handler.hh"
 #include "response_handler.hh"
 
+void int_handler(int sig)
+{
+    char c;
+
+    signal(sig, SIG_IGN);
+    printf("\nDo you really want to quit? [y/n] ");
+    c = getchar();
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Shutting down...");
+        http_server_stop();
+
+#if defined(_WIN32)
+        WSACleanup();
+#endif
+
+        printf("OK\n");
+
+        exit(0);
+    }
+    else
+    {
+        signal(SIGINT, int_handler);
+    }
+    getchar();
+}
+
 int main()
 {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = int_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
 
 #if defined(_WIN32)
     WSADATA d;
@@ -19,16 +53,13 @@ int main()
     }
 #endif
 
-    std::shared_ptr<HttpServer> httpServer = std::make_shared<HttpServer>();
+    SOCKET serverFd = http_server_init();
 
-    std::unique_ptr<EventManager> eventManager = std::make_unique<EventManager>(httpServer);
+    std::unique_ptr<EventManager> eventManager =
+        std::make_unique<EventManager>(serverFd);
 
     std::cout << "Listening on port: " << PORT << std::endl;
     eventManager->startEventLoop();
-
-#if defined(_WIN32)
-    WSACleanup();
-#endif
 
     return 0;
 }
