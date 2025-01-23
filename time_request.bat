e382b4281caa3ea2f0e14f666eb7bb89d909b8a8
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-23 12:17:14 +0000

    time requests in loop

--- time_request.bat
+++ time_request.bat
@@ -1,5 +1,24 @@
 @echo off
+
 setlocal
 FOR /F "eol=# tokens=*" %%i IN (%~dp0.env) DO SET %%i
-curl -w "@%~dp0curl-format.txt" -o NUL -s http://localhost:%PORT%
+
+set n = 5
+set "counter=0"
+
+:loop_start 
+    rem Your command here
+    echo (%counter%) Request... 
+    curl -w "@%~dp0curl-format.txt" -o NUL -s http://localhost:%PORT%
+    set /A counter=%counter%+1
+
+    rem Check condition to continue or exit the loop
+    if %counter% lss 5 (
+        goto loop_start 
+    ) else (
+        goto loop_end
+    )
+
+:loop_end
+
 endlocal
