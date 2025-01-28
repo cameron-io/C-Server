3770fee252beb9adc80739a1c3a437c575688742
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-28 01:02:43 +0000

    remove unnecessary windows nt macro

--- app/src/main.c
+++ app/src/main.c
@@ -2,9 +2,6 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0A00
-#endif
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 #else
